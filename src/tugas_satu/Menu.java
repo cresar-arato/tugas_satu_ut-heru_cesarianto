414c9d01ef670d69786c0e90830a74a561bac97b
cresar-arato <cesariantoheru@gmail.com>
2025-10-26 19:42:22 +0800

    Changes to be committed: 	modified:   bin/tugas_satu/Menu.class 	modified:   bin/tugas_satu/WarungJava.class 	modified:   src/tugas_satu/Menu.java 	modified:   src/tugas_satu/WarungJava.java

--- src/tugas_satu/Menu.java
+++ src/tugas_satu/Menu.java
@@ -5,7 +5,7 @@ class Menu {
 	double harga;
 	String kategori;
 
-	public  Menu(String namaitem, double harga, String kategori) {
+	public Menu(String namaitem, double harga, String kategori) {
 		this.namaitem = namaitem;
 		this.harga = harga;
 		this.kategori = kategori;
@@ -20,5 +20,18 @@ class Menu {
 	
 	public String getKategori() {
 		return kategori;
+	}
+
+	public static Menu[] getDaftarMenu() {
+		Menu[] daftarMenu = new Menu[8];
+		daftarMenu[0] = new Menu("Nasi Campur", 15000, "Makanan");
+		daftarMenu[1] = new Menu("Nasi Kuning", 17000, "Makanan");
+		daftarMenu[2] = new Menu("Lalapan", 18000, "Makanan");
+		daftarMenu[3] = new Menu("Soto", 25000, "Makanan");
+		daftarMenu[4] = new Menu("Teh", 3000, "Minuman");
+		daftarMenu[5] = new Menu("Susu", 5000, "Minuman");
+		daftarMenu[6] = new Menu("Kopi", 2000, "Minuman");
+		daftarMenu[7] = new Menu("Air", 1000, "Minuman");
+		return daftarMenu;
 	}
 }
--- src/tugas_satu/WarungJava.java
+++ src/tugas_satu/WarungJava.java
@@ -71,15 +71,7 @@ class WarungJava {
 			}
 		
 	public static void main(String[] args) {
-		Menu[] daftarMenu = new Menu[8];
-		daftarMenu[0] = new Menu("Nasi Campur", 15000, "Makanan");
-		daftarMenu[1] = new Menu("Nasi Kuning", 17000, "Makanan");
-		daftarMenu[2] = new Menu("Lalapan", 18000, "Makanan");
-		daftarMenu[3] = new Menu("Soto", 25000, "Makanan");
-		daftarMenu[4] = new Menu("Teh", 3000, "Minuman");
-		daftarMenu[5] = new Menu("Susu", 5000, "Minuman");
-		daftarMenu[6] = new Menu("Kopi", 2000, "Minuman");
-		daftarMenu[7] = new Menu("Air", 1000, "Minuman");
+		Menu[] daftarMenu = Menu.getDaftarMenu();
 		
 		tampilMenu(daftarMenu);
 		
